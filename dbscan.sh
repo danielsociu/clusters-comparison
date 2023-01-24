1d50ec6da6e20d9d73611df1f51ba51d917d9b8b
Daniel Sociu <danielsociu00@gmail.com>
2023-01-24 21:57:42 +0200

    update scripts

--- dbscan.sh
+++ dbscan.sh
@@ -1,4 +1,4 @@
-pyenv exec python3 resnet18_model.py \
+python3 unsupervised_clustering.py \
     --verbose \
     --mode 'random' \
     --feature-type 'resnet' \
@@ -8,3 +8,20 @@ pyenv exec python3 resnet18_model.py \
     # --type 'AGLOMERATIVE' \
     # --standard-scaler
     # --limit-train \
+
+# usage: unsupervised_clustering.py [-h] [--plot] [--verbose] [--grid-search] [--limit-train] [--standard-scaler] [--type {DBSCAN,AGLOMERATIVE}] [--feature-type {resnet,orb}]
+#                                   [--mode {unsupervised,random,supervised}]
+
+# optional arguments:
+#   -h, --help            show this help message and exit
+#   --plot                Whether to plot stuffs
+#   --verbose             Whether to print extra info
+#   --grid-search         Whether to use a grid-search strategy
+#   --limit-train         wether to limit train to same amount of data as val
+#   --standard-scaler     whether to scale the orb output
+#   --type {DBSCAN,AGLOMERATIVE}
+#                         Type of model to train
+#   --feature-type {resnet,orb}
+#                         Type of feature engineering
+#   --mode {unsupervised,random,supervised}
+#                         Type of training
